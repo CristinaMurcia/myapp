bdca072a1a0fc6c478a06180156b203b9e33ed2e
CristinaMurcia <167174172+CristinaMurcia@users.noreply.github.com>
2024-07-04 18:49:40 +0200

    septima

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', bookRoutes);
 
-app.get('app.js', (req, res)=> {
-    res.send("<h1>Hola caracola</h1>");
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Server is running on port ${PORT}');
